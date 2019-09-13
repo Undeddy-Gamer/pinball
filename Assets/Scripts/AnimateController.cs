ca6b34575f5108f0c330a6001e067851a2a7d2c2
Undeddy-Gamer <ed.dixen@gmail.com>
2019-09-13 15:07:31 +1000

    Finished game (not challenges)

---
+++ Assets/Scripts/AnimateController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateController : MonoBehaviour
+{
+    public Sprite[] spriteSet;
+    public float fps;
+    // #1
+    public GameObject treeObject;
+    private float turnspeed = 30f;
+    // #2
+
+    private void FixedUpdate()
+    {
+        //checks if current instance has treeObject
+        if (treeObject != null)
+        {
+            treeObject.transform.Rotate(new Vector3(0, 0, turnspeed * Time.deltaTime));  // Rotate the tree object
+        }
+    }
+}
---
+++ Assets/Scripts/Floatpiece.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Floatpiece : MonoBehaviour
+{
+    // Sound & Animation
+    public GameObject handcamObj;
+    public GameObject golightObj;
+    private SpriteRenderer handcamRenderer;
+    private SpriteRenderer golightRenderer;
+    private AnimateController handcamAniController;
+    private AnimateController golightAniController;
+    private SoundController sound;
+
+    // Score
+    private ScoreBoard scoreBoard;
+
+    // #1
+    private BuoyancyEffector2D floatEffector;
+    public float floatingTime = 0f; //floating duration
+    private float runningTime = 0f; //the current duration since startTime
+    private float startTime = 0f;
+
+    void Start()
+    {
+        // Get scoreboard and sound object
+        GameObject obj = GameObject.Find("scoreText");
+        scoreBoard = obj.GetComponent<ScoreBoard>();
+        sound = GameObject.Find("SoundObjects").GetComponent<SoundController>();
+        // Animation objects
+        handcamRenderer = handcamObj.GetComponent<Renderer>() as SpriteRenderer;
+        golightRenderer = golightObj.GetComponent<Renderer>() as SpriteRenderer;
+        handcamAniController = handcamObj.GetComponent<AnimateController>();
+        golightAniController = golightObj.GetComponent<AnimateController>();
+        //#2
+        floatEffector = GetComponent<BuoyancyEffector2D>();
+
+    }
+
+    void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (obj.name == "ball")
+        {
+            //#3
+            // on enter zone, start float
+            floatEffector.density = 50;
+            // start timer
+
+            if (startTime == 0f)
+            {
+                startTime = Time.time;
+                sound.bonus.Play();
+                scoreBoard.gamescore = scoreBoard.gamescore + 10;
+                golightRenderer.sprite = golightAniController.spriteSet[0];
+                StartCoroutine(BeginFloat());
+            }
+
+            sound.bonus.Play();
+            scoreBoard.gamescore = scoreBoard.gamescore + 10;
+            golightRenderer.sprite = golightAniController.spriteSet[0];
+            StartCoroutine(BeginFloat());
+
+        }
+    }
+
+    // #4
+
+
+    IEnumerator BeginFloat()
+    {
+        while (true)
+        {
+            //calculate current duration
+            runningTime = Time.time - startTime;
+
+            // Play animation loop
+            int index = (int)Mathf.PingPong(handcamAniController.fps * Time.time, handcamAniController.spriteSet.Length);
+            handcamRenderer.sprite = handcamAniController.spriteSet[index];
+            yield return new WaitForSeconds(0.1f);
+
+            // when time is up
+            if (runningTime >= floatingTime)
+            {
+                //stop effector reset timer
+                floatEffector.density = 0;
+                runningTime = 0f;
+                startTime = 0f;
+
+                // Stop & Reset Timer 
+                sound.bonus.Stop();
+                golightRenderer.sprite = golightAniController.spriteSet[1];
+                handcamRenderer.sprite = handcamAniController.spriteSet[handcamAniController.spriteSet.Length - 1];
+                break;
+            }
+        }
+    }
+
+        /*
+    IEnumerator BeginFloat()
+    {
+        while (true)
+        {
+            
+            // Play animation loop
+            int index = (int)Mathf.PingPong(handcamAniController.fps * Time.time, handcamAniController.spriteSet.Length);
+            handcamRenderer.sprite = handcamAniController.spriteSet[index];
+            yield return new WaitForSeconds(0.1f);
+
+            // Stop & Reset Timer 
+            sound.bonus.Stop();
+            golightRenderer.sprite = golightAniController.spriteSet[1];
+            handcamRenderer.sprite = handcamAniController.spriteSet[handcamAniController.spriteSet.Length - 1];
+            break;
+        } 
+    }*/
+}
+
